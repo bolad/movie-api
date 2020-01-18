e47fdb47952c8dc19adb8d280e58b247d888ec0e
stanley akyea <stanley.akyea@gmail.com>
2020-01-18 23:27:56 +0100

    Create movies controller and CRUD operations

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -19,3 +19,25 @@ class Api::V1::MoviesController < Api::V1::ApiController
       render json: @movie.errors, status: :unprocessable_entity
     end
   end
+
+  def update
+      if @movie.update(movie_params)
+        render json: @movie
+      else
+        render json: @movie.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @movie.destroy
+    end
+
+    private
+      def set_movie
+        @movie = Movie.find(params[:id])
+      end
+
+      def movie_params
+        params.require(:movie).permit(:title, :plot, :release_date)
+      end
+  end
