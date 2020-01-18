eef99676b1fdd45329238d17506bc87a50a25013
stanley akyea <stanley.akyea@gmail.com>
2020-01-18 23:05:10 +0100

    Create mpvies controller

---
+++ app/controllers/api/v1/movies_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::MoviesController < Api::V1::ApiController
+  before_action :set_movie, only: %i[show update destroy]
+
+  def index
+    @movies = Movie.all
+    render json: @movies
+  end
+
+  def show
+    render json: @movie
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      render json: @movie, status: :created
+    else
+      render json: @movie.errors, status: :unprocessable_entity
+    end
+  end
